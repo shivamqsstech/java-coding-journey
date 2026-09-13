d783670b1a6ace8ac3e9be52aa393fdf110325d3
Shivam Singh <Shivamsingh.bbdniit@gmail.com>
2026-09-13 10:29:23 +0530

    Add StringLenth program

---
+++ StringLength.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+class LengthCount{
+    public static void Count(String str){
+
+        int count =0;
+
+        for(char ch: str.toCharArray()){
+            count+=1;
+        }
+
+        System.out.println("The length of the strings is: " + count);
+    }
+
+}
+
+
+
+public class StringLength {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string whose length you want to count");
+        String str = sc.nextLine();
+        LengthCount.Count(str);
+
+
+    }
+    
+}
